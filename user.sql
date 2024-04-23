0a636e221440285983d91d884a5c59f1ecb133ec
fullm <matoma1_@naver.com>
2024-04-23 22:01:48 +0900

    sql문

---
+++ user.sql
@@ -0,0 +1,11 @@
+show databases;
+use mydb;
+select * from user;
+INSERT INTO user (name, age, email) VALUES
+('이름1', 30, 'name@example.com'),
+('김이박', 25, 'klp@example.com'),
+('Abc', 35, 'abb@example.com');
+
+INSERT INTO user (name, age) VALUES
+('namename', 32),
+('noemail', 11);
